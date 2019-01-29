cc4d046669e3244e44aa98fd2902f5676c4969e7
Shawn.J.Tseng (tw mis.twtc01.Newegg) 61878 <st7j@buyabs.corp>
2019-01-29 10:34:59 +0800

    watchify & log

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,29 @@
 var gulp = require('gulp');
-var ts = require('gulp-typescript');
-var tsProject = ts.createProject('tsconfig.json');
-
-// html檔案存放的原始路徑(編譯前)
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var watchify = require('watchify');
+var tsify = require('tsify');
+var gutil = require('gulp-util');
 var paths = {pages: ['src/*.html']};
 
-// 複製html檔到dist底下
+var watchedBrowserify = watchify(browserify({
+                                   basedir: '.',
+                                   debug: true,
+                                   entries: ['src/main.ts'],
+                                   cache: {},
+                                   packageCache: {}
+                                 }).plugin(tsify));
+
 gulp.task('copy-html', function() {
   return gulp.src(paths.pages).pipe(gulp.dest('dist'));
 });
 
-// gulp.task('default', gulp.series('copy-html', function() {
-//   return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest('dist'));
-// }));
-
-var browserify = require("browserify");
-var source = require('vinyl-source-stream');
-var tsify = require("tsify");
-gulp.task('default', gulp.series('copy-html', function() {
-  return browserify({
-           basedir: '.',
-           debug: true,
-           entries: ['src/main.ts'],
-           cache: {},
-           packageCache: {}
-         })
-      .plugin(tsify)
-      .bundle()
+function bundle() {
+  return watchedBrowserify.bundle()
       .pipe(source('bundle.js'))
       .pipe(gulp.dest('dist'));
-}));
+}
+
+gulp.task('default', gulp.series('copy-html', bundle));
+watchedBrowserify.on('update', bundle);
+watchedBrowserify.on('log', gutil.log);
